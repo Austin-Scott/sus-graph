e52f4961bccb05be5643e79939a4f2549dcb3cdc
Austin Scott <12504656+Austin-Scott@users.noreply.github.com>
2020-11-16 19:40:25 -0600

    Player code refactored

--- src/index.js
+++ src/index.js
@@ -8,23 +8,152 @@ cytoscape.use(cxtmenu)
 cytoscape.use(popper)
 
 
-function setOrUpdateNodeTooltip(ele) {
-    let state = ele.state
-    const stateText = `${state.alive?'':'Dead, '}${state.role}`
-    if(ele.tippy != undefined) {
-        ele.tippy.destroy()
-    }
+function makeTippy(node, text) {
+    var ref = node.popperRef()
+    // unfortunately, a dummy element must be passed
+    // as tippy only accepts a dom element as the target
+    // https://github.com/atomiks/tippyjs/issues/661
+    var dummyDomEle = document.createElement('div');
 
-    if(state.alive && state.role == 'unknown') {
-        return
-    }
+    var tip = tippy(dummyDomEle, {
+        onCreate: function (instance) { // mandatory
+            // patch the tippy's popper reference so positioning works
+            // https://atomiks.github.io/tippyjs/misc/#custom-position
+            instance.popperInstance.reference = ref;
+        },
+        lazy: false, // mandatory
+        trigger: 'manual', // mandatory
 
-    ele.tippy = makeTippy(ele, stateText)
-    ele.tippy.show()
+        // dom element inside the tippy:
+        content: function () { // function can be better for performance
+            var div = document.createElement('div');
+
+            div.innerHTML = text;
+
+            return div;
+        },
+
+        // your own preferences:
+        arrow: true,
+        placement: 'bottom',
+        hideOnClick: false,
+        multiple: true,
+        sticky: true,
+
+        // if interactive:
+        interactive: true,
+        appendTo: document.body // or append dummyDomEle to document.body
+    })
+
+    return tip
 }
 
+const KeyboardColorShortcuts = {
+    r: 'red',
+    b: 'blue',
+    g: 'green',
+    o: 'orange',
+    y: 'yellow',
+    w: 'white',
+    c: 'cyan',
+    l: 'lime',
+    i: 'pink',
+    a: 'black',
+    u: 'purple',
+    n: 'brown'
+}
+
+const PlayerColorValues = {
+    red: '#c51111',
+    blue: '#132ed1',
+    green: '#117f2d',
+    pink: '#ed54ba',
+    orange: '#ef7d0e',
+    yellow: '#F6F658',
+    black: '#3f474e',
+    white: '#d6e0f0',
+    purple: '#6b31bc',
+    brown: '#71491e',
+    cyan: '#38fedb',
+    lime: '#50ef39'
+}
+
+const Players = {}
+
+// Assumes the node has already been created and that player color = colorId
+function createPlayer(colorId, cy) {
+    let node = cy.nodes(`[id = "${colorId}"]`)
+
+    node.style('background-color', PlayerColorValues[colorId])
+    if (colorId == 'white') {
+        node.style('border-color', 'black')
+        node.style('border-width', '1')
+    }
+
+    let player = {
+        node: node,
+        inBoundEdges: [],
+        outBoundEdges: [],
+        active: false,
+        alive: true,
+        role: 'unknown',
+        colorId: colorId,
+        tippy: null,
+
+        setOrUpdateTooltip() {
+
+            const stateText = `${this.alive ? '' : 'Dead, '}${this.role}`
+            if (this.tippy != null) {
+                this.tippy.destroy()
+            }
+
+            if (this.alive && this.role == 'unknown') {
+                return
+            }
+
+            this.tippy = makeTippy(this.node, stateText)
+            this.tippy.show()
+
+        },
+        delete() {
+            if(this.tippy != null) {
+                this.tippy.destroy()
+            }
+            this.node.cy().remove(this.node)
+        },
+        toggleDead() {
+            this.alive = !this.alive
+            this.setOrUpdateTooltip()
+        },
+        markUnknown() {
+            this.role = 'unknown'
+            this.setOrUpdateTooltip()
+        },
+        markInposter() {
+            this.role = 'imposter'
+            this.setOrUpdateTooltip()
+        },
+        markCrewmate() {
+            this.role = 'crewmate'
+            this.setOrUpdateTooltip()
+        }
+    }
+
+    Players[colorId] = player
+}
+
+function deletePlayer(colorId) {
+    getPlayer(colorId).delete()
+    Players[colorId] = null
+}
+
+function getPlayer(colorId) {
+    return Players[colorId]
+}
+
+
 function removeElementTooltipIfNeeded(ele) {
-    if(ele.tippy != undefined) {
+    if (ele.tippy != undefined) {
         ele.tippy.destroy()
     }
 }
@@ -35,11 +164,11 @@ function setOrUpdateEdgeTooltip(ele, sus, reason) {
         reason: reason
     }
     const stateText = state.reason
-    if(ele.tippy != undefined) {
+    if (ele.tippy != undefined) {
         ele.tippy.destroy()
     }
 
-    if(state.sus) {
+    if (state.sus) {
         ele.style('target-arrow-color', 'red')
         ele.style('line-color', 'red')
     } else {
@@ -47,7 +176,7 @@ function setOrUpdateEdgeTooltip(ele, sus, reason) {
         ele.style('line-color', 'green')
     }
 
-    if(state.reason == 'sus' || state.reason == 'not sus') {
+    if (state.reason == 'sus' || state.reason == 'not sus') {
         return
     }
 
@@ -56,7 +185,7 @@ function setOrUpdateEdgeTooltip(ele, sus, reason) {
 }
 
 window.onEdgeAdd = function onEdgeAdd(ele) {
-    
+
 }
 
 window.initialize = function initialize(container) {
@@ -162,27 +291,18 @@ window.initialize = function initialize(container) {
         }
     })
 
-    cy.nodes('[id = "red"]').style('background-color', '#c51111')
-    cy.nodes('[id = "blue"]').style('background-color', '#132ed1')
-    cy.nodes('[id = "green"]').style('background-color', '#117f2d')
-    cy.nodes('[id = "pink"]').style('background-color', '#ed54ba')
-    cy.nodes('[id = "orange"]').style('background-color', '#ef7d0e')
-    cy.nodes('[id = "yellow"]').style('background-color', '#F6F658')
-    cy.nodes('[id = "black"]').style('background-color', '#3f474e')
-    cy.nodes('[id = "white"]').style('background-color', '#d6e0f0')
-    cy.nodes('[id = "white"]').style('border-color', 'black')
-    cy.nodes('[id = "white"]').style('border-width', '1')
-    cy.nodes('[id = "purple"]').style('background-color', '#6b31bc')
-    cy.nodes('[id = "brown"]').style('background-color', '#71491e')
-    cy.nodes('[id = "cyan"]').style('background-color', '#38fedb')
-    cy.nodes('[id = "lime"]').style('background-color', '#50ef39')
-
-    cy.nodes().forEach(node => {
-        node.state = {
-            alive: true,
-            role: 'unknown'
-        }
-    })
+    createPlayer('red', cy)
+    createPlayer('blue', cy)
+    createPlayer('green', cy)
+    createPlayer('pink', cy)
+    createPlayer('orange', cy)
+    createPlayer('yellow', cy)
+    createPlayer('black', cy)
+    createPlayer('white', cy)
+    createPlayer('purple', cy)
+    createPlayer('brown', cy)
+    createPlayer('cyan', cy)
+    createPlayer('lime', cy)
 
     cy.cxtmenu({
         selector: 'node',
@@ -190,45 +310,45 @@ window.initialize = function initialize(container) {
             {
                 content: 'Toggle Dead',
                 select: function (ele) {
-                    if (ele.state) {
-                        ele.state.alive = !ele.state.alive
-                        setOrUpdateNodeTooltip(ele)
+                    let colorId = ele.id()
+                    if (Object.keys(PlayerColorValues).includes(colorId)) {
+                        getPlayer(colorId).toggleDead()
                     }
                 }
             },
             {
                 content: 'Mark imposter',
                 select: function (ele) {
-                    if (ele.state) {
-                        ele.state.role = 'imposter'
-                        setOrUpdateNodeTooltip(ele)
+                    let colorId = ele.id()
+                    if (Object.keys(PlayerColorValues).includes(colorId)) {
+                        getPlayer(colorId).markInposter()
                     }
                 }
             },
             {
                 content: 'Mark crewmate',
                 select: function (ele) {
-                    if (ele.state) {
-                        ele.state.role = 'crewmate'
-                        setOrUpdateNodeTooltip(ele)
+                    let colorId = ele.id()
+                    if (Object.keys(PlayerColorValues).includes(colorId)) {
+                        getPlayer(colorId).markCrewmate()
                     }
                 }
             },
             {
                 content: 'Mark unknown',
                 select: function (ele) {
-                    if (ele.state) {
-                        ele.state.role = 'unknown'
-                        setOrUpdateNodeTooltip(ele)
+                    let colorId = ele.id()
+                    if (Object.keys(PlayerColorValues).includes(colorId)) {
+                        getPlayer(colorId).markUnknown()
                     }
                 }
             },
             {
                 content: 'Delete',
                 select: function (ele) {
-                    if (ele.state) {
-                        removeElementTooltipIfNeeded(ele)
-                        cy.remove(ele)
+                    let colorId = ele.id()
+                    if (Object.keys(PlayerColorValues).includes(colorId)) {
+                        deletePlayer(colorId)
                     }
                 }
             }
